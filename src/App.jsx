a5863a19f0831f3b6086ddcce90cee81da369c7e
MartinPSDev <martinperezsotelo.dev@gmail.com>
2023-08-17 15:55:30 -0300

    Primera version de react-task-aplication

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <>
+      <TaskForm />
+      <TaskList tasks={tasks} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/TaskList.jsx
@@ -0,0 +1,17 @@
+function TaskList ({tasks}) {
+  if (!tasks.length) {
+    return <h1>No hay tareas aun</h1>;
+  }
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
